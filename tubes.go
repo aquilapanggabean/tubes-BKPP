5552a6df2f049be53035d9416530f79ed214c314
aquilapanggabean <altair.panggabean@gmail.com>
2025-12-17 18:19:02 +0700

    Add files via upload

---
+++ tubes.go
@@ -0,0 +1,133 @@
+package main
+
+import "fmt"
+
+func main() {
+	var berat float64
+	var layanan string
+
+	// Tarif
+	tarifPerKg := 5000.0
+	tarifPerKm := 2000.0
+
+	// ArraY Kota
+	kota := []string{
+		"Jakarta",
+		"Karawang",
+		"Bandung",
+		"Ngawi",
+		"Banyuwangi",
+	}
+
+	posisi := []float64{
+		0,    // Jakarta
+		50,   // Karawang
+		150,  // Bandung
+		575,  // Ngawi
+		1140, // Banyuwangi
+	}
+
+	fmt.Println("KALKULATOR BIAYA PENGIRIMAN BARANG")
+
+	// Input berat
+	fmt.Print("Masukkan berat barang (kg): ")
+	fmt.Scan(&berat)
+	if berat <= 0 {
+		fmt.Println("ERROR: Berat harus > 0")
+		return
+	}
+	// Pilih kota asal
+	fmt.Println("\nPilih Kota Asal:")
+	for i, k := range kota {
+		fmt.Printf("%d. %s\n", i+1, k)
+	}
+
+	var KotaAsal int
+	fmt.Print("Masukkan pilihan: ")
+	fmt.Scan(&KotaAsal)
+
+	if KotaAsal < 1 || KotaAsal > len(kota) {
+		fmt.Println("ERROR: Kota asal tidak valid")
+		return
+	}
+	KotaAsal--
+
+	// Pilih kota tujuan
+	fmt.Println("\nPilih Kota Tujuan:")
+	for i, k := range kota {
+		fmt.Printf("%d. %s\n", i+1, k)
+	}
+
+	var KotaTujuan int
+
+	fmt.Print("Masukkan pilihan: ")
+	fmt.Scan(&KotaTujuan)
+
+	if KotaTujuan < 1 || KotaTujuan > len(kota) {
+		fmt.Println("ERROR: Kota tujuan tidak valid")
+		return
+	}
+	KotaTujuan--
+
+	if KotaAsal == KotaTujuan {
+		fmt.Println("ERROR: Kota asal dan tujuan tidak boleh sama")
+		return
+	}
+
+	// Hitung Jarak
+	jarak := posisi[KotaAsal] - posisi[KotaTujuan]
+	if jarak < 0 {
+		jarak = -jarak
+	}
+
+	// Pilih layanan
+	fmt.Println("\nPilih layanan:")
+	fmt.Println("1. Reguler")
+	fmt.Println("2. Express")
+	fmt.Println("3. Cargo")
+	fmt.Print("Masukkan pilihan: ")
+	fmt.Scan(&layanan)
+
+	biayaDasar := (berat * tarifPerKg) + (jarak * tarifPerKm)
+
+	var faktor float64
+	var estimasiHari float64
+
+	switch layanan {
+	case "1":
+		faktor = 1.0
+		estimasiHari = jarak / 300
+	case "2":
+		faktor = 1.5
+		estimasiHari = jarak / 500
+	case "3":
+		if berat < 5 {
+			fmt.Println("ERROR: Cargo minimal 5 kg")
+			return
+		}
+		faktor = 0.8
+		estimasiHari = jarak / 200
+	default:
+		fmt.Println("ERROR: Layanan tidak valid")
+		return
+	}
+
+	total := biayaDasar * faktor
+
+	// Estimasi waktu
+	var estimasiOutput string
+	if estimasiHari < 1 {
+		estimasiOutput = fmt.Sprintf("%.1f Jam", estimasiHari*24)
+	} else {
+		estimasiOutput = fmt.Sprintf("%.1f Hari", estimasiHari)
+	}
+
+	// Output
+	fmt.Println("\nRINCIAN PENGIRIMAN")
+	fmt.Printf("Asal          : %s\n", kota[KotaAsal])
+	fmt.Printf("Tujuan        : %s\n", kota[KotaTujuan])
+	fmt.Printf("Jarak         : %.0f km\n", jarak)
+	fmt.Printf("Berat         : %.1f kg\n", berat)
+	fmt.Printf("Total Biaya   : Rp %.0f\n", total)
+	fmt.Printf("Estimasi Waktu: %s\n", estimasiOutput)
+}
